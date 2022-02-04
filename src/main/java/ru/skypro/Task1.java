a96c066fa6490d6a77406902b540400783e27a9b
Olga22S <22031992Les>
2022-02-04 14:55:12 +0300

    HW 7 is done

---
+++ src/main/java/ru/skypro/Task1.java
@@ -0,0 +1,10 @@
+package ru.skypro;
+
+import java.util.List;
+
+public class Task1 {
+
+    public static void printOddNums(List<Integer> nums) {
+        nums.stream().filter(s -> s % 2 != 0).forEach(s -> System.out.print(s + " "));
+    }
+}
